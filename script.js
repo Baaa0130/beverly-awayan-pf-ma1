76cd3c97f2bae0fad640311f5943421cf3ea57b3
Baaa0130 <69729099+Baaa0130@users.noreply.github.com>
2020-09-20 19:49:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//Question 1
+var firstVar;
+
+//Question 2
+var name = "Bhevz";
+console.log(name);
+
+//Question 3
+var num = 12345678;
+console.log(num);
+
+//Question 4
+var division = 20 / 5;
+
+//Question 5
+console.log(typeof "frog");
+// expected output: “string”
+
+//Question 6
+var orderHasShipped = new Boolean();
+
+//Question 7
+if (orderHasShipped = true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+//Question 8
+for (var i = 0; i < 10; i++) {
+   console.log(i);
+}
